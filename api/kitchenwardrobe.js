7471842b8341fc4453fae6021e0484746699813b
dhruvraj <dhruvrajsinghsolanki161@gmail.com>
2022-02-23 21:42:42 +0530

    fix:path chnage

--- api/kitchenwardrobe.js
+++ api/kitchenwardrobe.js
@@ -10,7 +10,7 @@ mongoose
     dbName: "Enquiry",
     useNewUrlParser: true,
     tls: true,
-    tlsCAFile: "../1.crt",
+    tlsCAFile: __dirname + "../libs/1.crt",
   })
   .then(() => {
     console.log("dbconnected");
