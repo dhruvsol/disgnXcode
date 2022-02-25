39770b0e69450ed7d0fbcea44dbc4bf9c6b46b3d
dhruvraj <dhruvrajsinghsolanki161@gmail.com>
2022-02-25 09:56:15 +0530

    feat:added the path

--- libs/valueretailer.js
+++ libs/valueretailer.js
@@ -5,19 +5,15 @@ const { Schema } = mongoose;
 const retailer = new Schema({
   name: {
     type: String,
-    required: true,
   },
   number: {
     type: Number,
-    required: true,
   },
   email: {
     type: String,
-    required: true,
   },
   enquiry: {
     type: String,
-    required: true,
   },
 });
 export default mongoose.model("retailer", retailer);
