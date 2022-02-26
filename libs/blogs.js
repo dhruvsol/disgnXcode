7793a548ccf10726d4d476fc73ba03009f6933a3
dhruvraj <dhruvrajsinghsolanki161@gmail.com>
2022-02-26 19:20:45 +0530

    fix:change the api

--- libs/blogs.js
+++ libs/blogs.js
@@ -4,15 +4,12 @@ const { Schema } = mongoose;
 const blogs = new Schema({
   heading: {
     type: String,
-    required: true,
   },
   subheading: {
     type: String,
-    required: true,
   },
   image: {
     type: String,
-    required: true,
   },
 });
 export default mongoose.model("blogs", blogs);
