5694ab93a5cb5fa47098eb17fd0ff32b831057d5
dhruvraj <dhruvrajsinghsolanki161@gmail.com>
2022-03-01 20:33:20 +0530

    fix:blogs page api

--- src/pages/Blogs/Blogs.jsx
+++ src/pages/Blogs/Blogs.jsx
@@ -7,7 +7,7 @@ import { BlogCard } from "./Card/BlogCard";
 import axios from "axios";
 import { data } from "autoprefixer";
 export const Blogs = () => {
-  const blog = axios.get("/api/blogs");
+  const blog = axios.get("/api/fetch");
   console.log(blog);
   const settings = {
     dots: false,
